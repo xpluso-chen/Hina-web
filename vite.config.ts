c79846f6d5253a9d7988ebda45b75040f769a467
xpluso <xxxplusooo913@gmail.com>
2025-02-08 16:47:46 +0800

    新增SCSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/src/assets/styles/variables.scss" as *;`+`@use "/src/assets/styles/extend.scss" as *;`
+        
+      }
+    }
+  }
 })

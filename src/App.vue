b9fb2b8afbda3255ea66a7c78566c891a7adbcf2
xpluso <xxxplusooo913@gmail.com>
2025-02-10 12:14:22 +0800

    試切換class效果

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-
   <Test />
-
   <div>
     <Test2 />
   </div>
@@ -22,41 +20,41 @@
       </div>
 
       <div class="btn-box">
-        <button class="btn2" @click="toggleClass"><span>我是首頁按鈕2號</span></button>
+        <!-- 使用 ref 綁定到這個按鈕元素 -->
+        <button class="btn2" ref="btn2" @click="toggleClass"><span>我是首頁按鈕2號</span></button>
       </div>
 
     </div>
     <div class="main-area-footer">留白</div>
   </div>
-
 </template>
 
 <script setup lang="ts">
-import Test from './components/api.vue'
-import Test2 from './components/motto.vue'
-import Test3 from './components/joke.vue'
+import { ref, nextTick } from 'vue';
+import Test from './components/api.vue';
+import Test2 from './components/motto.vue';
+import Test3 from './components/joke.vue';
+
+const btn2 = ref<HTMLElement | null>(null);
 
 function toggleClass() {
-      $(".btn2").addClass("active");
+  nextTick(() => {
+    if (btn2.value) {
+      btn2.value.classList.toggle('active');
     }
-
+  });
+}
 </script>
 
 <style lang="scss">
 @use "sass:map";
 
 .a {
-  // 貼底
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
 }
 
-// .btn-box{
-//  position: absolute;
-//   top: -100px;
-//   left: -100px;
-// }
 .btn1 {
   position: relative;
   outline: none;
@@ -71,36 +69,21 @@ function toggleClass() {
   width: 210px;
   opacity: 1;
   background-color: #ffffff;
-  border: 2px solid map.get($color, black );
+  border: 2px solid map.get($color, black);
 
   &:hover {
     animation: rotate 0.7s ease-in-out both;
-
     @keyframes rotate {
-      0% {
-        transform: rotate(0deg) translate3d(0, 0, 0);
-      }
-
-      25% {
-        transform: rotate(3deg) translate3d(0, 0, 0);
-      }
-
-      50% {
-        transform: rotate(-3deg) translate3d(0, 0, 0);
-      }
-
-      75% {
-        transform: rotate(1deg) translate3d(0, 0, 0);
-      }
-
-      100% {
-        transform: rotate(0deg) translate3d(0, 0, 0);
-      }
+      0% { transform: rotate(0deg) translate3d(0, 0, 0); }
+      25% { transform: rotate(3deg) translate3d(0, 0, 0); }
+      50% { transform: rotate(-3deg) translate3d(0, 0, 0); }
+      75% { transform: rotate(1deg) translate3d(0, 0, 0); }
+      100% { transform: rotate(0deg) translate3d(0, 0, 0); }
     }
   }
 
   span {
-    color: map.get($color, black );
+    color: map.get($color, black);
     font-size: 14px;
     font-weight: 700;
     letter-spacing: 1px;
@@ -110,34 +93,18 @@ function toggleClass() {
       animation-delay: 0.06s;
 
       @keyframes storm {
-        0% {
-          transform: translate3d(0, 0, 0) translateZ(0);
-        }
-
-        25% {
-          transform: translate3d(4px, 0, 0) translateZ(0);
-        }
-
-        50% {
-          transform: translate3d(-3px, 0, 0) translateZ(0);
-        }
-
-        75% {
-          transform: translate3d(2px, 0, 0) translateZ(0);
-        }
-
-        100% {
-          transform: translate3d(0, 0, 0) translateZ(0);
-        }
+        0% { transform: translate3d(0, 0, 0) translateZ(0); }
+        25% { transform: translate3d(4px, 0, 0) translateZ(0); }
+        50% { transform: translate3d(-3px, 0, 0) translateZ(0); }
+        75% { transform: translate3d(2px, 0, 0) translateZ(0); }
+        100% { transform: translate3d(0, 0, 0) translateZ(0); }
       }
     }
   }
-
 }
 
 .active {
-    background-color: red;
-    color: yellow;
-  }
-
+  background-color: red;
+  color: yellow;
+}
 </style>

c4bbab43ff3fe5c6ef1e3552a45f1acd1f7c289f
xpluso <xxxplusooo913@gmail.com>
2025-02-12 11:34:28 +0800

    設定.Vue可用的顏色變數在ts

---
+++ src/constants/colorMap.ts
@@ -0,0 +1,18 @@
+export const colorMap = {
+    black:"#252526",
+    white:"#F5F5F5",
+    yellow:"#EFE2C0",
+    deepYellow:"#E6A649",
+    orange:"#F07145",
+    ttiffany:"#66BAB7",
+    pink:"#FFDBCF",
+    perple:"#A0A3C9",
+    green:"#C7CD9D",
+    
+    deepYellowHover:"#F07145",
+    ttiffanyHover:"#A0A3C9",
+    pinkHover:"#F5F5F5",
+    orangeHover:"#E6A649",
+    greenHover:"#EFE2C0"
+  };
+  
